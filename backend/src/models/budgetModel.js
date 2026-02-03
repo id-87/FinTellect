ee09826ec38c07970c4942eda8d3a56a3cf39b24
Harshit Gupta <hrstgpt87@gmail.com>
2026-02-03 10:11:27 +0530

    Bud mod

--- backend/src/models/budgetModel.js
+++ backend/src/models/budgetModel.js
@@ -1,6 +1,10 @@
-_id
-        userID
-        category
-        monthlyLimit
-        createdAt
-        updatedAt
+
+const mongoose=require('mongoose')
+const model=new mongoose.Schema({
+    _id:{},
+    userID:{},
+    category:{},
+    monthlyLimit:{},
+    createdAt:{},
+    updatedAt:{}
+})
